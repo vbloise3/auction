ceb51620a8ce5134d5c46554b31ce3b4618eeea6
Vince Bloise <vbloise3@verizon.net>
2017-01-04 21:31:42 -0500

    Added rating to product #2

--- app/services/product-service.ts
+++ app/services/product-service.ts
@@ -141,5 +141,13 @@ var reviews = [
         "user": "User 6",
         "rating": 5,
         "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
+    },
+    {
+        "id": 6,
+        "productId": 1,
+        "timestamp": "2014-05-20T11:42:00+00:00",
+        "user": "User 9",
+        "rating": 4,
+        "comment": "Aenean vestibulum velit id placerat posuere. Praesent placerat mi ut massa tempor, sed rutrum metus rutrum. Fusce lacinia blandit ligula eu cursus. Proin in lobortis mi. Praesent pellentesque auctor dictum. Nunc volutpat id nibh quis malesuada. Curabitur tincidunt luctus leo, quis condimentum mi aliquet eu. Vivamus eros metus, convallis eget rutrum nec, ultrices quis mauris. Praesent non lectus nec dui venenatis pretium."
     }
 ];
